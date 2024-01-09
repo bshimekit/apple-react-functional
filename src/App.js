fb18b0db724ca843f8833668c1f975753a8717eb
Bereket Shimekit(Bek) <bereket@chadez.co.za>
2024-01-09 13:32:57 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Header from "./components/Header"
+import AlertSection from "./components/mainSection/AlertSection";
+import FirstSection from "./components/mainSection/FirstSection";
+import SecondSection from "./components/mainSection/SecondSection";
+import ThirdSectionSection from "./components/mainSection/ThirdSection";
+import FourthSection from "./components/mainSection/FourthSection";
+import FifthSection from "./components/mainSection/FifthSection";
+import SixthSection from "./components/mainSection/SixthSection";
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <AlertSection />
+      <FirstSection />
+      <SecondSection />
+      <ThirdSectionSection />
+      <FourthSection />
+      <FifthSection />
+      <SixthSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App
